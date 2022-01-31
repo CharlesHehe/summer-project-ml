f88836ab035dbccdec96023ea464ccf23afb941b
CharlesHehe <hechenjuner@gmail.com>
2022-01-31 14:19:47 +1300

    add operating binary file feature

--- preprocessing.py
+++ preprocessing.py
@@ -1,9 +1,51 @@
 from numpy import genfromtxt
 from numpy import unique
 from numpy import savez_compressed
+import numpy as np
+import codecs
+import binascii
 
-X = genfromtxt("raw.txt", delimiter=",")
-print(X.shape)
-X = unique(X, axis=0)
+# X = genfromtxt("raw.txt", delimiter=",")
+# X = genfromtxt("raw2.txt")
+# with open('raw2.txt', encoding='cp1252', 'rb') as f:
+# with open('raw2.txt', encoding="utf8", errors='ignore') as f:
+#     for line in f:
+#         print(line)
+# dtype = np.dtype('B')
+# with open('raw2.txt', "rb") as f:
+# binary_data = f.read()
+# text = binary_data.decode('utf-8')
+# text = binascii.hexlify(binary_data)
+# text = codecs.decode(binary_data, 'base64')
+# numpy_data = np.fromfile(f, dtype)
+# data = f.read()
+# print(numpy_data)
 
+# print(X.shape)
+# X = unique(X, axis=0)
+
+# savez_compresseddata.npz', X)('
+
+
+import struct
+
+struct_fmt = 'i'+'f' * 6  # int[5], float, byte[255]
+struct_len = struct.calcsize(struct_fmt)
+struct_unpack = struct.Struct(struct_fmt).unpack_from
+results = []
+
+with open("raw.txt", "rb") as f:
+    while True:
+        data = f.read(28)
+        if not data:
+            break
+        s = struct_unpack(data)
+        results.append(s)
+X = unique(results, axis=0)
 savez_compressed('data.npz', X)
+# file = open("raw.txt", "rb")
+# while 1:
+#     c = struct.unpack('i', file.read(4))
+#     f = struct.unpack('f' * 6, file.read(4 * 6))
+#
+#     print(c, f)
--- scikit-learn-logistic-regression.py
+++ scikit-learn-logistic-regression.py
@@ -3,6 +3,10 @@ from joblib import dump, load
 from sklearn.model_selection import train_test_split
 from random import randint
 from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
 
 model_file_name = "logistic-regression.joblib"
 
@@ -25,23 +29,21 @@ y = data['arr_0'][:, 0]
 random_value = randint(0, 10)
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=random_value)
 
-from sklearn.linear_model import LogisticRegression
+
+
 # define the model: LogisticRegression
 # model = LogisticRegression(random_state=random_value).fit(X_train, y_train)
 
 # assign a cluster to each example
 # y_prediction = model.predict(X_test)
 # model.predict_proba(X_test)
-from sklearn.svm import SVC
-from sklearn.pipeline import make_pipeline
-from sklearn.preprocessing import StandardScaler
-
-# model = make_pipeline(StandardScaler(), SVC(gamma='auto'))
-# model.fit(X_train, y_train)
 
 
-gnb = GaussianNB()
-model = gnb.fit(X_train, y_train)
+model = make_pipeline(StandardScaler(), SVC(gamma='auto'))
+model.fit(X_train, y_train)
+
+# gnb = GaussianNB()
+# model = gnb.fit(X_train, y_train)
 # test model
 accuracy = model.score(X_test, y_test)
 print(accuracy)
