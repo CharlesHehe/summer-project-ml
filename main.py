c75ae489b9acf6bdf74230adc755d302fbe58b40
CharlesHehe <hechenjuner@gmail.com>
2022-02-13 21:20:40 +1300

    add initialize_train_model function into main.py

--- main.py
+++ main.py
@@ -1,16 +1,88 @@
-# This is a sample Python script.
+from numpy import load as np_load
+from joblib import dump, load
+from sklearn.model_selection import train_test_split
+from random import randint
+from sklearn.naive_bayes import GaussianNB
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import SGDClassifier
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+def save_model(clf):
+    model_name = type(clf).__name__
+    dump(clf, f"./models/{model_name}.joblib")
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+def load_model(model_file_name):
+    clf = load(model_file_name)
+    return clf
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def initialize_train_model(model_name, X_train, y_train):
+    model_ft = None
+    if model_name == "sgd":
+        """ SGDClassifier
+        """
+        model_ft = SGDClassifier(loss='modified_huber', shuffle=True, random_state=101)
+        model_ft.fit(X_train, y_train)
+    if model_name == "knn":
+        """ KNeighborsClassifier
+        """
+        model_ft = KNeighborsClassifier(n_neighbors=15)
+        model_ft.fit(X_train, y_train)
+    if model_name == "logistic_regression":
+        """ LogisticRegression
+        """
+        model_ft = LogisticRegression(random_state=random_value)
+        model_ft.fit(X_train, y_train)
+    if model_name == "decision_tree":
+        """ DecisionTreeClassifier
+        """
+        model_ft = DecisionTreeClassifier(max_depth=10, random_state=101,
+                                          max_features=None, min_samples_leaf=15)
+        model_ft.fit(X_train, y_train)
+    if model_name == "random_forest":
+        """RandomForestClassifier
+        """
+        model_ft = RandomForestClassifier(n_estimators=70, oob_score=True, n_jobs=1,
+                                          random_state=101, max_features=None, min_samples_leaf=30)
+        model_ft.fit(X_train, y_train)
+    if model_name == "svm":
+        """SVM
+        """
+        model_ft = SVC(kernel="linear", C=0.025, random_state=101)
+        model_ft.fit(X_train, y_train)
+    if model_name == "naive_bayes":
+        """Naive Bayes
+        """
+        model_ft = GaussianNB()
+        model_ft.fit(X_train, y_train)
+    return model_ft
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+model_name = sys.argv[1]
+
+# read dataset
+buttons = np_load('train_data.npz')
+# # training dataset
+X_buttons = buttons['arr_0'][:, 1:]
+# # testing dataset
+y_buttons = buttons['arr_0'][:, 0]
+random_value = randint(0, 10)
+X_train, X_test, y_train, y_test = train_test_split(X_buttons, y_buttons, test_size=0.25, random_state=random_value)
+
+model = initialize_train_model(model_name, X_train, y_train)
+
+# test model
+accuracy = model.score(X_test, y_test)
+print(f"training: {accuracy}")
+
+# save model
+save_model(model)
+# load model
+model_name = type(model).__name__
+load_model(f"./models/{model_name}.joblib")
--- preprocessing.py
+++ preprocessing.py
@@ -1,73 +1,25 @@
-from numpy import genfromtxt
 from numpy import unique
 from numpy import savez_compressed
-import numpy as np
-import codecs
-import binascii
-
-# X = genfromtxt("raw.txt", delimiter=",")
-# X = genfromtxt("raw2.txt")
-# with open('raw2.txt', encoding='cp1252', 'rb') as f:
-# with open('raw2.txt', encoding="utf8", errors='ignore') as f:
-#     for line in f:
-#         print(line)
-# dtype = np.dtype('B')
-# with open('raw2.txt', "rb") as f:
-# binary_data = f.read()
-# text = binary_data.decode('utf-8')
-# text = binascii.hexlify(binary_data)
-# text = codecs.decode(binary_data, 'base64')
-# numpy_data = np.fromfile(f, dtype)
-# data = f.read()
-# print(numpy_data)
-
-# print(X.shape)
-# X = unique(X, axis=0)
-
-# savez_compresseddata.npz', X)('
-
-
+import os
 import struct
 
 struct_fmt = 'i' + 'f' * 6  # int[5], float, byte[255]
 struct_len = struct.calcsize(struct_fmt)
 struct_unpack = struct.Struct(struct_fmt).unpack_from
 results = []
-buttons = []
-bandsaw = []
+train_data = []
 
-# , open("bandsaw.txt", "rb") as f2
-with open("training.txt", "rb") as f1, open("bandsaw.txt", "rb") as f2:
-    while True:
-        data = f1.read(28)
-        if not data:
-            break
-        s = list(struct_unpack(data))
-        if s[4:] == [0.0, 0.0, 0.0]:
-            s[0] = 0
-            buttons.append(s)
-        else:
-            s[0] = 1
-            buttons.append(s)
-    while True:
-        data = f2.read(28)
-        if not data:
-            break
-        s = list(struct_unpack(data))
-        if s[4:] == [0.0, 0.0, 0.0]:
-            s[0] = 0
-            bandsaw.append(s)
-        else:
-            s[0] = 1
-            bandsaw.append(s)
+files = os.listdir('./raw_data')
 
-buttons = unique(buttons, axis=0)
-bandsaw = unique(bandsaw, axis=0)
-savez_compressed('buttons.npz', buttons)
-savez_compressed('bandsaw.npz', bandsaw)
-# file = open("raw.txt", "rb")
-# while 1:
-#     c = struct.unpack('i', file.read(4))
-#     f = struct.unpack('f' * 6, file.read(4 * 6))
-#
-#     print(c, f)
+for file in files:
+    with open(f"./raw_data/{file}", "rb") as f1:
+        while True:
+            data = f1.read(28)
+            if not data:
+                break
+            s = list(struct_unpack(data))
+            train_data.append(s)
+
+train_data = unique(train_data, axis=0)
+
+savez_compressed('train_data.npz', train_data)
